a200390d6c8cc4e35725caba6f0d2a4e4da85745
Aye Chan Wai <ayechanwai.edu@gmail.com>
2024-01-17 22:47:43 +0630

    sending data between fragments

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Sending Data Between Fragment"
+include(":app")
